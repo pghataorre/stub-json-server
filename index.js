e8a48da4984c418a27e8853de786cce5ac12f7c6
pghataorre <pghataorre@gmail.com>
2025-07-02 15:33:48 +0100

    down graded vesion of json-server

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const jsonServer = require("json-server");
-
+const jsonServer = require('json-server');
+const path = require('path')
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
+
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001
 
 server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
-    console.log('JSON SERVER is running ------------- ON PORT: ', port);
-});
+  console.log('JSON Server is running on port: ', port);
+})
